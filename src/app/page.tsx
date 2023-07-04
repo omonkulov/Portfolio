fbdcc11ad3c926907135ce0d5a13684b5e6f8f82
Sardorbek Omonkulov <somonkulov2018@gmail.com>
2023-07-04 01:35:56 -0400

    Added a hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from "next/image";
+import styles from "./page.module.css";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      
-    </main>
-  )
+    <div className={styles.root}>
+      <div className={styles.heroContainer}>
+        <div className={styles.heroLeft}>
+          <h2>Hello, I'm</h2>
+          <h1>Sardorbek Omonkulov</h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <div className={styles.buttons}>
+            <button className={styles.buttonAboutMe}>About Me</button>
+            <button className={styles.buttonResume}>Download Resume</button>
+          </div>
+        </div>
+        <div className={styles.heroRight}>
+          <Hero />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import style from "../styles/navbar.module.css";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className={style.root}>
+      <div className={style.logo}>
+        <p className='primary-text'>Sardorbek Omonkulov</p>
+      </div>
+      <div className={style.links}>
+        <p>Home</p>
+        <p>About</p>
+        <p>Projects</p>
+        <p>Contact</p>
+      </div>
+    </div>
   )
 }
 
