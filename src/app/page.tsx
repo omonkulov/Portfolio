2dcf0371a0b860f35ec85d0748399ae616f91cf4
Sardorbek Omonkulov <somonkulov2018@gmail.com>
2023-07-04 20:14:42 -0400

    Hero and navbar components are responsive now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,25 +5,7 @@ import Hero from "@/components/Hero";
 export default function Home() {
   return (
     <div className={styles.root}>
-      <div className={styles.heroContainer}>
-        <div className={styles.heroLeft}>
-          <h2>Hello, I'm</h2>
-          <h1>Sardorbek Omonkulov</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
-            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-            aliquip ex ea commodo consequat.
-          </p>
-          <div className={styles.buttons}>
-            <button className={styles.buttonAboutMe}>About Me</button>
-            <button className={styles.buttonResume}>Download Resume</button>
-          </div>
-        </div>
-        <div className={styles.heroRight}>
-          <Hero />
-        </div>
-      </div>
+      <Hero/>
     </div>
   );
 }
